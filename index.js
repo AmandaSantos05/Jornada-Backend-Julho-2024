dd5cac88bbd26c5d53f1a0288d3f4b11a45ea9f0
Amanda Santos <amanda280721@gmail.com>
2024-07-03 15:50:25 -0400

    Endpoint Listar nomes

--- index.js
+++ index.js
@@ -9,5 +9,10 @@ app.get('/oi', (req, res) => {
     res.send('Olá Mundo!')
 })
 
+const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith'];
+
+app.get('/item', (req, res) => {
+    res.send(lista)
+})
 
 app.listen(3000)

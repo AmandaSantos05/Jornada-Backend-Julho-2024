16d72f6dbe93961c3f837ad0f88fc183ebf96054
Amanda Santos <amanda280721@gmail.com>
2024-07-04 08:42:57 -0400

    Endpoint upddate

--- index.js
+++ index.js
@@ -5,31 +5,38 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
-})
+});
 
 app.get('/oi', (req, res) => {
     res.send('Olá Mundo!');
-})
+});
 
 const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith'];
 
 app.get('/item', (req, res) => {
     res.send(lista);
-})
+});
 
 app.post('/item', (req, res) => {
     const item = req.body.nome;
     lista.push(item);
-    res.send('Item criado com sucesso!')
+    res.send('Item criado com sucesso!');
 });
 
 app.get('/item/:id', (req, res) => {
-    const id = req.params.id
+    const id = req.params.id;
 
-    const item = lista[id - 1]
-    res.send(item)
+    const item = lista[id - 1];
+    res.send(item);
+});
 
-    console.log(id)
+app.put('/item/:id', (req, res) => {
+    const id = req.params.id;
+
+    const novoItem = req.body.nome;
+
+    lista[id - 1] = novoItem;
+    res.send('Item atualizado com sucesso!');
 })
 
 app.listen(3000);
